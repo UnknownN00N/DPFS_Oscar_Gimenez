0fa799991e471b688694240b4661f92e4529146d
UknownN00N <federicogimenez99@hotmail.com>
2024-12-06 20:39:05 -0300

    express added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.listen(PORT, () =>
+console.log("Server is running in: " + "http://localhost:" + PORT));
