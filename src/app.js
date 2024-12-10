6330a29def25af7426fedf72b1789a9b7066e624
UknownN00N <federicogimenez99@hotmail.com>
2024-12-10 16:44:58 -0300

    added structure to productDetail & last css's

--- src/app.js
+++ src/app.js
@@ -4,10 +4,26 @@ const path = require('path');
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public/')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req,res) => {
     return res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/product-detail', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'productDetail.html'))
+})
+
+app.get('/cart', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'productCart.html'))
+})
+
+app.get('/register', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'register.html'))
+})
+
+app.get('/login', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'login.html'))
 })
 
 app.listen(PORT, () =>
