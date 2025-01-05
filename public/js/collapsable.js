b8d56a697eccd0a148e3e8bdececed3e51ea0143
UknownN00N <federicogimenez99@hotmail.com>
2025-01-05 01:04:40 -0300

    partials added to all views

--- public/js/collapsable.js
+++ public/js/collapsable.js
@@ -1,20 +1,4 @@
-/*var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "flex") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "flex";
-    }
-  });
-}
-*/
-
-var coll = document.getElementsByClassName("subCategories__collapsible");
+var coll = document.querySelectorAll(".subCategories__collapsible, .productDetail__collapsible, .productDetail__requirementCollapsible");
 
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
