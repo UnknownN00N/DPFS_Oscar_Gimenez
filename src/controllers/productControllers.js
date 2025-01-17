197998249203af6b8bf6862b7b9c35ba58ddae60
UknownN00N <federicogimenez99@hotmail.com>
2025-01-17 02:55:09 -0300

    added first items partial & functionality

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -9,7 +9,11 @@ const productController = {
 
     cart : (req, res) =>{
         return res.render('products/productCart');
-    }
+    },
+    getPartial: (req, res) => {
+        const id = req.query.id; // Recibir el ID desde la solicitud
+        res.render('partials/itemVideos', { id }); // Asegúrate del nombre y ubicación del archivo
+      },
 };
 
 module.exports = productController;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {show, create, cart} = require('../controllers/productControllers.js');
+const {show, create, cart, getPartial} = require('../controllers/productControllers.js');
 
 router 
 .get ('/product-detail', show)
 .get ('/add', create)
 .get ('/cart', cart)
+.get('/partial', getPartial);
+
 
 
 module.exports = router;
