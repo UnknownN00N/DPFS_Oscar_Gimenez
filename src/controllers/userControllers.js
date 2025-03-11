7a0c6e3767250f81001183d1f1b32a11411524ef
UnknownN00N <federicogimenez99@hotmail.com>
2025-03-11 16:02:44 -0300

    register form functionality partially added

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,9 +3,32 @@ const userController = {
         return res.render('users/login');
     },
     register : (req, res) =>{
-        return res.render('users/register');
-    },
-   
-};
+        let users = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/users.json')));
+  
+          return res.render('/register');
+      },
+  
+  //Parsea el Json, extrae el último objeto, recibe el nuevo producto generado por el formulario de
+  // create, pushea el nuevo producto y convierte el nuevo archivo en un JSON
+      save : (req, res) =>{
+        let users = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/users.json')));
+        let lastUser = users.pop();
+        users.push(lastUser);
+        let newUser = {
+          user_id: lastUser.user_id +1,
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          type: user,
+          //avatar: req.file.filename,
+        }
+
+       users.push(newUser);
+       let newUserSave = JSON.stringify(users, null, 2);
+       fs.writeFileSync(path.resolve(__dirname, '../database/users.json'), newUserSave);
+       //redirecciona a una ruta deseada
+       res.redirect('/');
+        },
+}
 
 module.exports = userController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path')
 
-const {login, register} = require('../controllers/userControllers.js');
+const {login, register, save} = require('../controllers/userControllers.js');
+
+//Subir el archivo usando multer y dónde guardarlo
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/database/images/users'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'user' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  
+  const upload = multer({ storage })
 
 router 
 .get ('/login', login)
 .get ('/register', register)
+.post('/register', save) //upload.single('avatar'), save)
+
 
 
 module.exports = router;
