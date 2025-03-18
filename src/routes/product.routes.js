e45bf13fff517858e3cc624c19fbb83f86e416da
UnknownN00N <federicogimenez99@hotmail.com>
2025-03-18 16:22:15 -0300

    minor authentication added to products

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,14 +7,15 @@ const {show, create, cart, getPartial, catalog, save, edit, update, destroy} = r
 
 //Subir el archivo usando multer (Con su disposición como middleware)
 const { uploadProd } = require('../middlewares/multer')
+const guestAuth = require('../middlewares/guestAuth.js')
 
 router 
 .get ('/products/detail/:courseid', show)
-.get ('/products/add', create)
+.get ('/products/add', guestAuth, create)
 .post('/products/add', uploadProd.single('courseimage'), save)
-.get('/products/edit/:courseid', edit)
+.get('/products/edit/:courseid', guestAuth, edit)
 .put('/products/edit/:courseid', uploadProd.single('courseimage'), update)
-.get('/products/delete/:courseid', destroy)
+.get('/products/delete/:courseid', guestAuth, destroy)
 
 .get('/cart', cart)
 .get('/partial', getPartial)
